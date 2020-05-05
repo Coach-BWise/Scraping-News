271802406d7c954ce6a774cc9429a2c62f70ea50
Coach-BWise <wisebry11@gmail.com>
2020-05-05 13:17:04 -0400

    commit to heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Initialize Express
 var app = express();
